85a7a18f4e859582c653d77570d9c6dfe3eebe7c
Phil Blundell <philb@brightsign.biz>
2021-05-17 16:52:14 +0100

    interrupts: Add separate IDE mask location
    ADFS doesn't preserve the rest of the register contents when enabling
    or disabling interrupts, it just writes either 0x20 or 0x00 to the
    defined mask location.  So we need to make sure that doing so doesn't
    clobber anything else.

--- fpga/interrupts.v
+++ fpga/interrupts.v
@@ -9,39 +9,44 @@ module interrupts(irq, fiq, econet_fiq, ethernet_irq, ide_irq, uart_tx_irq, uart
    output fiq;
 
    inout [7:0]  D;
-   input [13:0] A;
+   input [13:2] A;
    input 	cs;
    input 	re;
    input 	we;
    input 	reset;
 
-   assign irq = ((ethernet_irq && mask[6]) || (ide_irq && mask[5]) || (uart_tx_irq && mask[4]) || (uart_rx_irq && mask[3]));
+   assign irq = ((ethernet_irq && mask[6]) || (ide_irq && mask[5]) || (uart_tx_irq && mask[4]) || (uart_rx_irq && mask[3])) || mask[2];
    assign fiq = econet_fiq && mask[7];
 
    wire [7:0] status;
-   assign status[7:0] = { econet_fiq, ethernet_irq, ide_irq, uart_tx_irq, uart_rx_irq, 1'b0, fiq, irq };
+   assign status[7:0] = { econet_fiq, ethernet_irq, ide_irq, uart_tx_irq, uart_rx_irq, 1'b1, fiq, irq };
 
    wire       mask_cs;
    wire       status_cs;
    wire       soft_reset_cs;
+   wire	      ide_mask_cs;
 
    assign mask_cs = cs && (A[3:2] == 2'b01);
    assign status_cs = cs && (A[3:2] == 2'b00);
+   assign ide_mask_cs = cs && (A[3:2] == 2'b10);
    assign soft_reset_cs = cs && (A[3:2] == 2'b11);
 
-   reg [7:3]  mask;
-   always @(negedge (mask_cs && we) or posedge reset)
+   reg [7:2]  mask;
+   always @(negedge we or posedge reset)
      begin
 	if (reset)
 	  begin
-	     mask[7:3] <= 5'b11111;
+	     mask[7:2] <= 6'b000000;
 	  end
 	else
 	  begin
-	     mask[7:3] <= D[7:3];
+	     if (mask_cs)
+	       mask[7:2] <= D[7:2];
+	     else if (ide_mask_cs)
+	       mask[5] <= D[5];
 	  end
      end
 
-   assign D[7:0] = (status_cs && re) ? status[7:0] : (mask_cs && re) ? { mask[7:3], 3'b000 } : 8'bzzzzzzzz;
+   assign D[7:0] = (status_cs && re) ? status : (mask_cs && re) ? { mask[7:2], 2'b00 } : 8'bzzzzzzzz;
 
 endmodule // interrupts
